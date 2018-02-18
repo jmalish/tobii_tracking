59f846d14b6c5fc6084e3db537d9d95232878aae
jmalish <jordanmalish@gmail.com>
2018-02-18 15:12:13 -0500

    Initial upload

---
+++ tobii_tracker/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Tobii.Interaction;
+
+namespace tobii_tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool testing = true;
+
+            var host = new Host();
+            var gazePointDataStream = host.Streams.CreateGazePointDataStream();
+            
+            Console.Write("Please enter name of file to be created (Leave empty to name as date): "); 
+
+            #region write full data to file
+            string fileName = Console.ReadLine();
+            if (fileName == "")
+            {
+                fileName = DateTime.Now.ToString("dd_mm_yyyy_hh_mm_ss");
+            }
+
+            string fileLocation = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            Directory.CreateDirectory(fileLocation + "\\eyetracks");
+            if (!testing)
+            {
+                string myFile = fileLocation + "\\eyetracks\\" + fileName + ".csv";
+
+                StreamWriter sw = new StreamWriter(myFile);
+
+                Console.WriteLine($"Writing data to {myFile}");
+
+                sw.WriteLine("Timestamp,X,Y");
+                gazePointDataStream.GazePoint(
+                (x, y, ts) =>
+                {
+                    //Console.WriteLine($"{ts},{x},{y}");
+                    sw.WriteLine($"{ts},{x},{y}");
+                });
+                #endregion
+
+                Console.WriteLine("\nPress any key to stop recording and parse data...");
+                Console.ReadKey();
+
+                sw.Close();
+                host.DisableConnection();
+            }
+
+            try
+            {
+                Parser parser = new Parser(fileLocation, fileName);
+                if (parser.ParseData())
+                {
+                    Console.WriteLine("\nData parsed, press any key to exit...");
+                } else
+                {
+                    Console.WriteLine("\nSomething went wrong, data may not have been parsed" +
+                        "\nPress any key to exit...");
+                }
+                Console.ReadKey();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("\nSomething went wrong: " + exc.Message +
+                    "\nSome or all data may not have been parsed" +
+                    "\nPress any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ tobii_tracker/parser.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace tobii_tracker
+{
+    class Parser
+    {
+        public string FileLocation { get; set; }
+        public string FileName { get; set; }
+
+        public Parser(string _fileLoc, string _fileName)
+        {
+            FileLocation = _fileLoc;
+            FileName = _fileName;            
+        }
+
+        public bool ParseData()
+        {
+            int[,] coords = new int[1920,1080];
+
+            string myFile = FileLocation + "\\eyetracks\\" + FileName + ".csv";
+
+            Console.WriteLine("Reading file {0}", myFile);
+
+
+            
+            using (StreamReader sr = new StreamReader(myFile))
+            {
+                string line;
+                sr.ReadLine(); // skip first line (contains headers)
+                while ((line = sr.ReadLine()) != null)
+                {
+                    int x = Decimal.ToInt32(Math.Round(decimal.Parse(line.Split(',')[1]))); // just, uhh, look somewhere else
+                    int y = Decimal.ToInt32(Math.Round(decimal.Parse(line.Split(',')[2]))); // seriously, please
+
+                    // set floor and ceiling for both x and y
+                    if (x < 1)
+                    {
+                        x = 1;
+                    }
+                    else if (x > 1920)
+                    {
+                        x = 1919;
+                    }
+
+                    if (y < 1)
+                    {
+                        y = 1;
+                    }
+                    else if (y > 1080)
+                    {
+                        y = 1079;
+                    }
+
+                    x = x-1;
+                    y = y-1;
+
+
+
+                    coords[x, y] = coords[x, y] + 1;
+                }
+
+                Console.WriteLine("Completed reading file");
+            }
+
+            int test = coords.GetLength(0);
+            int test2 = coords.GetLength(1);
+
+
+            string parsedFile = FileLocation + "\\eyetracks\\" + FileName + "_parsed.json";
+            Console.WriteLine("File parsed, writing parsed data to {0}", parsedFile);
+
+            StreamWriter sw = new StreamWriter(parsedFile);
+
+            sw.Write("[");
+            for (int x = 0; x < coords.GetLength(0); x++)
+            {
+                sw.Write("[");
+                for (int y = 0; y < coords.GetLength(1); y++)
+                {
+                    sw.Write(coords[x, y]);
+                    if (y != coords.GetLength(1) - 1)
+                    {
+                        sw.Write(",");
+                    }
+                }
+                sw.Write("]");
+                if (x != coords.GetLength(0) - 1)
+                {
+                    sw.Write(",");
+                }
+            }
+            sw.Write("]");
+
+            sw.Close();
+
+            return true;
+        }
+    }
+}
+
+
+
+
+
+//sw.Write("[");
+//            for (int y = 0; y<coords.GetLength(1); y++) // y
+//            {
+//                sw.Write("[");
+//                for (int x = 0; x<coords.GetLength(1); x++) // x
+//                {
+//                    // Console.WriteLine("{0}, {1}: {2}", x, y, coords[x, y]);
+//                    sw.Write(coords[x, y]);
+//                    if (x != coords.GetLength(1) - 1)
+//                    {
+//                        sw.Write(",");
+//                    }
+//                }
+//                sw.Write("]");
+//                if (y != coords.GetLength(0) - 1)
+//                {
+//                    sw.Write(",");
+//                }
+//            }
+//            sw.Write("]");
