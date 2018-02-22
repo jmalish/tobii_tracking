4efe6a44a6b91c3b71985b0c8514f2db266f7490
jmalish <jordanmalish@gmail.com>
2018-02-22 16:53:28 -0500

    Add comments to program.cs

--- tobii_tracker/Program.cs
+++ tobii_tracker/Program.cs
@@ -13,48 +13,47 @@ namespace tobii_tracker
         {
             bool testing = false; // setting this to true skips the eye recording part, useful for testing the parser
 
-            var host = new Host();
-            var gazePointDataStream = host.Streams.CreateGazePointDataStream();
+            var host = new Host(); // create tobii host
+            var gazePointDataStream = host.Streams.CreateGazePointDataStream(); // create data stream from host
             
             Console.Write("Please enter name of file to be created (Leave empty to name as date): "); 
 
             #region write full data to file
-            string fileName = Console.ReadLine();
-            if (fileName == "")
+            string fileName = Console.ReadLine(); // get file name from user
+            if (fileName == "") // if the file name is empty
             {
-                fileName = DateTime.Now.ToString("dd_mm_yyyy_hh_mm_ss");
+                fileName = DateTime.Now.ToString("dd_mm_yyyy_hh_mm_ss"); // just name the file the current date and time
             }
 
-            string fileLocation = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-            Directory.CreateDirectory(fileLocation + "\\eyetracks");
+            string fileLocation = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory); // get desktop
+            Directory.CreateDirectory(fileLocation + "\\eyetracks"); // create the 'eyetracks' directory on the desktop, if it exists this is ignored
             if (!testing)
             {
-                string myFile = fileLocation + "\\eyetracks\\" + fileName + ".csv";
+                string myFile = fileLocation + "\\eyetracks\\" + fileName + ".csv"; // create string to point to file
 
-                StreamWriter sw = new StreamWriter(myFile);
+                StreamWriter sw = new StreamWriter(myFile); // create stream writer
 
-                Console.WriteLine($"Writing data to {myFile}");
+                Console.WriteLine($"Writing data to {myFile}"); // notify user of file name
 
-                sw.WriteLine("Timestamp,X,Y");
+                sw.WriteLine("Timestamp,X,Y"); // set header in file
                 gazePointDataStream.GazePoint(
                 (x, y, ts) =>
                 {
-                    //Console.WriteLine($"{ts},{x},{y}");
-                    sw.WriteLine($"{ts},{x},{y}");
+                    sw.WriteLine($"{ts},{x},{y}"); // this takes the current time, the x and y coords, and writes them to the file
                 });
                 #endregion
 
-                Console.WriteLine("\nPress any key to stop recording and parse data...");
-                Console.ReadKey();
+                Console.WriteLine("\nPress any key to stop recording and parse data..."); // tell user how to stop data recording
+                Console.ReadKey(); // when a key is pressed stop reading
 
-                sw.Close();
-                host.DisableConnection();
+                sw.Close(); // close the stream writer
+                host.DisableConnection(); // close the tobii host
             }
 
             try
             {
-                Parser parser = new Parser(fileLocation, fileName);
-                if (parser.ParseData())
+                Parser parser = new Parser(fileLocation, fileName); // create parser
+                if (parser.ParseData()) // tell the parser to do it's thing, it returns a bool that basically says if it was succesful or not
                 {
                     Console.WriteLine("\nData parsed, press any key to exit...");
                 } else
@@ -64,7 +63,7 @@ namespace tobii_tracker
                 }
                 Console.ReadKey();
             }
-            catch (Exception exc)
+            catch (Exception exc) // basic error handling
             {
                 Console.WriteLine("\nSomething went wrong: " + exc.Message +
                     "\nSome or all data may not have been parsed" +
